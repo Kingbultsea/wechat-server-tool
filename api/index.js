31621800d5a3a873d00cca98f86076c73fb7c989
譜 <894275157@qq.com>
2021-06-14 16:01:20 +0800

    api

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 const app = require('express')()
-const convert = require('../dist/api/index')
+const { v4 } = require('uuid')
 
-app.get('/wechat_open_platform/preauthcode', (req, res) => {
+app.get('/api', (req, res) => {
+    const path = `/api/item/${v4()}`
     res.setHeader('Content-Type', 'text/html')
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-    res.end('????')
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
+})
+
+app.get('/api/item/:slug', (req, res) => {
+    const { slug } = req.params
+    res.end(`Item: ${slug}`)
 })
 
 module.exports = app
