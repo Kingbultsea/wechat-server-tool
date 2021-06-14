a7df5d25e06a9047ebdce6e370736da653d30a9e
譜 <894275157@qq.com>
2021-06-14 15:52:15 +0800

    feat: api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const app = require('express')()
 const convert = require('../dist/api/index')
 
 app.get('/wechat_open_platform/preauthcode', (req, res) => {
+    res.setHeader('Content-Type', 'text/html')
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
     res.end('????')
 })
 
