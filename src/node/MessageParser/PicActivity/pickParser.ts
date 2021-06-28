8e95dcef17f888f440b8192d6f48414790f1d6e8
譜 <894275157@qq.com>
2021-06-28 16:24:45 +0800

    msg

--- src/node/MessageParser/PicActivity/pickParser.ts
+++ src/node/MessageParser/PicActivity/pickParser.ts
@@ -16,11 +16,12 @@ async function sendMediaDataCopy({ targetInfo, uid, root, frameName = [], dir }:
 
         // 获取用户信息头像
         const userInfo = await getUserInfo({ serveAccessToken: targetInfo.authorizer_access_token, uid, platFormName: targetInfo.name  })
-        activityFlow({ userInfo, formData, targetInfo, uid, resolve, frameName, index: 0, root, dir })
+        activityFlow({ userInfo, formData, targetInfo, uid, resolve, frameName, index: 0, root, dir, pairage: true, step: 1 })
+        activityFlow({ userInfo, formData, targetInfo, uid, resolve, frameName, index: 1, root, dir, pairage: false, step: 1 })
     })
 }
 
-async function activityFlow({ userInfo, formData, targetInfo, uid, resolve, frameName, index = 0, root, dir }: any = {}) {
+async function activityFlow({ userInfo, formData, targetInfo, uid, resolve, frameName, index = 0, root, dir, pairage, step }: any = {}) {
     let resultPath: any = ''
     if (userInfo && userInfo.picUrl) {
         resultPath = await parseBlockTypeAvatar({ root, frameName: frameName[index] + '.png', userPicUrl: (userInfo || {}).picUrl, dir  })
@@ -54,7 +55,7 @@ async function activityFlow({ userInfo, formData, targetInfo, uid, resolve, fram
             // 发送消息给用户
             sendMediaContent(uid, JSON.parse(body).media_id, targetInfo.authorizer_access_token, 'image')
             if (frameName.length > index + 1) {
-                activityFlow({ userInfo, formData, targetInfo, uid, resolve, frameName, index: index + 1, root, dir })
+                activityFlow({ userInfo, formData, targetInfo, uid, resolve, frameName, index: pairage ? (2 * step) : (2 * step) + 1 , root, dir, step: step + 1 })
             }
         }
 
