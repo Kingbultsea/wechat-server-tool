3faa4e5fe1fa1feb59f0e9b290d254d5d2540a99
AbaAba~ <894275157@qq.com>
2021-07-31 04:21:53 +0800

    Delete dist/node directory

--- dist/node/util.d.ts
+++
@@ -1,8 +0,0 @@
-declare const getPostData: (ctx: any) => Promise<string>;
-declare const writeFile: (ROOT: string | undefined, data: Record<any, any>) => void;
-declare const getData: (ctx: any, encrypt: any, tagName?: string | undefined) => Promise<{
-    result: any;
-    bodyXML: any;
-}>;
-export declare function randomString(len: number): string;
-export { getPostData, writeFile, getData };
