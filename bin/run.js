3c35e3ba4d738ff8c625232fbf333d5042ef5f4a
kingbultsea <894275157@qq.com>
2023-02-21 12:06:37 +0800

    fix: fix run.js

--- bin/run.js
+++ bin/run.js
@@ -4,96 +4,100 @@ const argv = require('minimist')(process.argv.slice(2))
 const path = require('path')
 
 try {
-  // todo 创建模板
-  if (process.argv.includes('create')) {
-    if (!argv.appid || !argv.url) {
-      console.log('\x1B[31m%s\x1B[0m', '请配置appid或url参数')
-      return
-    }
-    createTemp({ appid: argv.appid, url: argv.url })
-  } else {
-    let json
-    //  3@CZ9RXf3-x_y_$
-    try {
-      json = require(path.join(process.cwd(), './config.json'))
-    } catch (e) {
-      console.log('请配置config.json文件')
-      return
-    }
+  function start() {
+    // todo 创建模板
+    if (process.argv.includes('create')) {
+      if (!argv.appid || !argv.url) {
+        console.log('\x1B[31m%s\x1B[0m', '请配置appid或url参数')
+        return
+      }
+      createTemp({ appid: argv.appid, url: argv.url })
+    } else {
+      let json
+      //  3@CZ9RXf3-x_y_$
+      try {
+        json = require(path.join(process.cwd(), './config.json'))
+      } catch (e) {
+        console.log('请配置config.json文件')
+        return
+      }
 
-    if (argv.test) {
-      global.__TEST__ = true
-    }
+      if (argv.test) {
+        global.__TEST__ = true
+      }
 
-    if (
-      ['appid', 'secret', 'encodingAESKey', 'token'].some((e) => {
-        if (json && json.wechat && !json.wechat[e]) {
-          return true
-        } else if (!(json && json.wechat) && !argv[e]) {
-          return true
+      if (
+        ['appid', 'secret', 'encodingAESKey', 'token'].some((e) => {
+          if (json && json.wechat && !json.wechat[e]) {
+            return true
+          } else if (!(json && json.wechat) && !argv[e]) {
+            return true
+          }
+          return false
+        })
+      ) {
+        console.error('请正确配置appid secret plugins encodingAESKey token')
+        return
+      }
+
+      const config = json.wechat || argv || {}
+
+      config.input = json.input
+
+      config.data = json.data
+
+      console.log(config, argv, json)
+
+      global.__CONFIG__ = config
+
+      try {
+        // 磁盘数据
+        config.DATA = require(path.join(process.cwd(), json.data))
+      } catch (e) {
+        // 无则创建
+        if (!config.DATA) {
+          const temp = {
+            self: {
+              Encrypt: ''
+            },
+            thirdPart: []
+          }
+
+          config.DATA = temp
+
+          fs.writeFile(
+            path.join(process.cwd(), path.relative(process.cwd(), json.data)),
+            JSON.stringify(temp)
+          )
         }
-        return false
+      }
+
+      const server = require('../dist/node').createServer(config)
+
+      let port = argv.port || 3000
+      server.on('error', (e) => {
+        if (e.code === 'EADDRINUSE') {
+          console.log(`port ${port} is in use, trying another one...`)
+          setTimeout(() => {
+            server.close()
+            server.listen(++port)
+          }, 100)
+        } else {
+          console.error(e)
+        }
       })
-    ) {
-      console.error('请正确配置appid secret plugins encodingAESKey token')
-      return
+
+      server.on('listening', () => {
+        console.log(`Running at http://localhost:${port}`)
+      })
+
+      server.listen(port)
+
+      module.exports = server
     }
-
-    const config = json.wechat || argv || {}
-
-    config.input = json.input
-
-    config.data = json.data
-
-    console.log(config, argv, json)
-
-    global.__CONFIG__ = config
-
-    try {
-      // 磁盘数据
-      config.DATA = require(path.join(process.cwd(), json.data))
-    } catch (e) {
-      // 无则创建
-      if (!config.DATA) {
-        const temp = {
-          self: {
-            Encrypt: ''
-          },
-          thirdPart: []
-        }
-
-        config.DATA = temp
-
-        fs.writeFile(
-          path.join(process.cwd(), path.relative(process.cwd(), json.data)),
-          JSON.stringify(temp)
-        )
-      }
-    }
-
-    const server = require('../dist/node').createServer(config)
-
-    let port = argv.port || 3000
-    server.on('error', (e) => {
-      if (e.code === 'EADDRINUSE') {
-        console.log(`port ${port} is in use, trying another one...`)
-        setTimeout(() => {
-          server.close()
-          server.listen(++port)
-        }, 100)
-      } else {
-        console.error(e)
-      }
-    })
-
-    server.on('listening', () => {
-      console.log(`Running at http://localhost:${port}`)
-    })
-
-    server.listen(port)
-
-    module.exports = server
   }
+
+  start();
 } catch (e) {
   console.log(e)
 }
